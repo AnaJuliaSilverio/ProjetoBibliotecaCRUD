b1deb94e21a5b3bdde0f8d4f235c9be30dedae0d
AnaJuliaSilverio <ana.silverio@zup.com.br>
2024-02-16 16:18:46 -0300

    coomit

---
+++ projetoBiblioteca/k8s/chamando.sh
@@ -0,0 +1,1 @@
+for i in {1..10000}; do curl http://simpleapi.com/book; done
--- projetoBiblioteca/monitoring-resources/create_stack.sh
+++ projetoBiblioteca/monitoring-resources/create_stack.sh
@@ -3,3 +3,5 @@
 kubectl create ns monitoring
 helm upgrade --install prom prometheus-community/kube-prometheus-stack -n monitoring --values values.yaml
 kubectl apply -f servicemonitor.yaml
+
+kubectl apply -f alert-rules.yaml
